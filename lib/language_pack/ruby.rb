3cdf686ffdd549b790b947c736d81bd144ffe44c
John Maxwell <jgwmaxwell@gmail.com>
2014-01-09 12:22:11 +0000

    More debugging

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -392,6 +392,7 @@ WARNING
           topic "Installing #{g}"
           @fetchers[:buildpack].fetch_untar("#{g}.tgz")
         end
+        topic "Uncompressed: #{`ls -laR`}"
         Dir["bin/*"].each {|path| run("chmod 755 #{path}") }
       end
       topic "Slug vendor base is #{slug_vendor_base}"
